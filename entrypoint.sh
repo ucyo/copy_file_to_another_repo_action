f902011907a19d9c60cf5c9ac655549d60d2b6dd
ucyo <cayoglu@me.com>
2022-03-27 18:20:23 +0200

    Enable proper folder copy to root

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,22 +27,27 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-if [ ! -z "$INPUT_RENAME" ]
-then
-  echo "Setting new filename: ${INPUT_RENAME}"
-  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
-else
-  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
-fi
+# if [ ! -z "$INPUT_RENAME" ]
+# then
+#   echo "Setting new filename: ${INPUT_RENAME}"
+#   DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
+# else
+#   DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+# fi
+
+echo $(pwd)
+
+echo $(ls -al .)
+
+echo $(ls public)
 
 echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 if [ -z "$INPUT_USE_RSYNC" ]
 then
-  cp -R "$INPUT_SOURCE_FILE" "$DEST_COPY"
+  cp -R $INPUT_SOURCE_FILE/* "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
 else
   echo "rsync mode detected"
-  rsync -avrh "$INPUT_SOURCE_FILE" "$DEST_COPY"
+  rsync -avrh $INPUT_SOURCE_FILE/* "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
 fi
 
 cd "$CLONE_DIR"
